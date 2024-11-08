5bd0c48cd286d13581677f83e2caea4df988291b
sbstndb <sebastienduboid4@gmail.com>
2024-11-08 09:25:54 +0100

    Update xtensor

---
+++ build/src/xtensor/CMakeFiles/xt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xt.dir/xtensor.cpp.o"
+  "CMakeFiles/xt.dir/xtensor.cpp.o.d"
+  "xt"
+  "xt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
